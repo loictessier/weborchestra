9c96b79dd64123610f11282e027a385f2c0daf8a
Loïc Tessier <loictessier.inge@gmail.com>
2020-08-04 21:44:03 +0200

    Add database service: 	- new docker-compose.yml config file to access postgresql + pgadmin services 	- new dependency in pipfile -> psycogpg2 to handle database access 	- modified settings to add database access credentials, settings has been modified to prepare for the use of differents settings files for multiples environments

--- weborchestra/settings/__init__.py
+++ weborchestra/settings/__init__.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'weborchestra.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'weborchestra',
+        'USER': 'adm_weborchestra',
+        'PASSWORD': 'adm_weborchestra_pwd',
+        'HOST': 'localhost',
+        'PORT': '15432',
     }
 }
 
@@ -103,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr'
 
 TIME_ZONE = 'UTC'
 
