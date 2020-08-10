81dc2c5bfbfec78d8efb80582a5db847f11167dd
Loïc Tessier <loictessier.inge@gmail.com>
2020-08-10 12:01:55 +0200

    First functional test specced out in comments, and now uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,28 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
- 
-assert 'Django' in browser.title
+class NewUserTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_create_an_account_and_get_confirmation_message(self):
+        # user check out home page
+        self.browser.get('http://localhost:8000')
+        
+        # he notices the page title and header mention web'orchestra
+        self.assertIn("Web'Orchestra", self.browser.title)
+        self.fail('Finish the test!')
+
+        # he clicks the sign up button and is redirected to the sign up form
+
+        # he complete the form with his username, mail and password
+
+        # when he hits enter, the account is created and he is redirected to the front page
+        # the user reads a confirmation message that confirm his account was created
+
+if __name__ =='__main__':
+    unittest.main(warnings='ignore')
