3a409fb0732cf83f9cc35c820fd8bdce76839c6a
Loïc Tessier <loictessier.inge@gmail.com>
2020-08-30 10:55:49 +0200

    [config] Fix STATIC_ROOT settings

--- weborchestra/settings/__init__.py
+++ weborchestra/settings/__init__.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Logging configuration
 # https://docs.djangoproject.com/en/3.0/topics/logging/
