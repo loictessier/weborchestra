a4b1055284a16e62302a2b3bf7be5600071a1ff4
Loïc Tessier <loictessier.inge@gmail.com>
2021-04-20 18:54:58 +0200

    [US-20b] Updated footer to add legals and link to the facebook pages with some other informations.

--- core/views.py
+++ core/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'core/index.html')
+
+
+def legal_notice(request):
+    return render(request, 'core/legals.html')
